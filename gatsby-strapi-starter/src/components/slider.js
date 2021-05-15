6846f4cb6f3c8accf9c3edc15487c9d50aa5ebce
spaceC00kie <kirstie317@gmail.com>
2021-05-14 22:41:52 -0400

    changed color

--- gatsby-strapi-starter/src/components/slider.js
+++ gatsby-strapi-starter/src/components/slider.js
@@ -7,7 +7,7 @@ import "../../node_modules/slick-carousel/slick/slick.css"
 import "../../node_modules/slick-carousel/slick/slick-theme.css"
 
 const SliderContainer = styled.div`
-  background: linear-gradient(210deg, #943cff 0%, #dd45d3 40.13%, #fc9a57 90%);
+  background: linear-gradient(210deg, #943cff 0%, #dd45d3 40.13%, #396ff7 90%);
   color: white;
   padding: 10rem 0;
   clip-path: polygon(0 0, 100% 0, 100% 85%, 0% 100%);
