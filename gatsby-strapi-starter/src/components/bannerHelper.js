df28156ca06151ca5991055a12a27a7d7d1d46dc
spaceC00kie <kirstie317@gmail.com>
2021-05-14 21:35:13 -0400

    picked template

---
+++ gatsby-strapi-starter/src/components/bannerHelper.js
@@ -0,0 +1,43 @@
+import React from "react"
+import Link from "./link"
+import styled from "styled-components"
+import NumberFormat from "react-number-format"
+
+let StyledLink = styled((props) => <Link {...props} />)`
+  color: black;
+  &:hover {
+    color: black;
+  }
+`
+
+const HeaderItem = ({ text, title, Icon, url, klass, type }) => (
+  <div className={klass + " align-items-center flex mr-3"}>
+    <Icon className="mr-2" />
+    <span>
+      {title ? title + ": " : ""}
+      {url ? (
+        <StyledLink to={url}>
+          {type === "address" ? (
+            text
+          ) : (
+            <NumberFormat
+              value={text}
+              displayType={"text"}
+              format="#### ### ###"
+            />
+          )}
+        </StyledLink>
+      ) : (
+        text
+      )}
+    </span>
+  </div>
+)
+
+const IconLink = ({ href, Icon }) => (
+  <Link to={href} className="flex items-center">
+    <Icon size="1.2em" className="mr-2" />
+  </Link>
+)
+
+export { HeaderItem, IconLink }
---
+++ gatsby-strapi-starter/src/components/btn.js
@@ -0,0 +1,38 @@
+import React from "react"
+import Link from "./link"
+import styled from "styled-components"
+
+let Button = styled.div`
+  -webkit-appearance: unset !important;
+  border-radius: 0.35rem;
+  background: ${(props) => props.theme.primary};
+  box-shadow: 0 0.5rem 1.5rem rgba(22, 28, 45, 0.1);
+  color: white;
+  display: inline-block;
+  padding: 0.8125rem 1.25rem;
+  transition-duration: 0.25s;
+  font-size: 1.2rem;
+  border: none;
+  &:hover {
+    transform: translateY(-3px);
+    box-shadow: 0 1rem 2.5rem rgba(22, 28, 45, 0.1),
+      0 0.5rem 1rem -0.75rem rgba(22, 28, 45, 0.1);
+  }
+`
+
+let Btn = ({ to, children, ...props }) => {
+  if (to) {
+    return (
+      <Link to={to}>
+        <Button {...props}>{children}</Button>
+      </Link>
+    )
+  }
+  return (
+    <Button as="button" {...props}>
+      {children}
+    </Button>
+  )
+}
+
+export default Btn
