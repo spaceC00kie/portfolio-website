5e77548dec6423edf0a91eb875d6ee5b29a0e4f2
spaceC00kie <kirstie317@gmail.com>
2021-05-27 13:12:35 -0400

    subfooter dark

--- gatsby-strapi-starter/src/components/subFooter.js
+++ gatsby-strapi-starter/src/components/subFooter.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Container } from "reactstrap"
 
 let SubFooter = ({ title }) => (
-  <div className="bg-light">
+  <div className="bg-dark">
     <Container className="pb-3">
       <span className="text-secondary small">
         Copyright © {new Date().getFullYear()}. {title}. All rights reserved.
