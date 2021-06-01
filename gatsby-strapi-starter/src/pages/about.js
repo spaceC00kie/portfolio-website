86eb087d7fce8c2554f85cfd37c7b99ff4e214e3
spaceC00kie <kirstie317@gmail.com>
2021-06-01 14:14:41 -0400

    about

--- gatsby-strapi-starter/src/pages/about.js
+++ gatsby-strapi-starter/src/pages/about.js
@@ -4,22 +4,13 @@ import PageTemplate from "../components/pageTemplate"
 let About = () => (
   <PageTemplate title="About Me">
     <p>
-      I'm a programmer. For real? Yeah. Who'd a thunk it?
+      I'm a programmer.
     </p>
     <p>
-      My journey began when I made a website to help me get a job in the customer service field.
-      The experience was surprisingly fun and I noticed more people were contacting me for interviews.
+    My journey began when I made a website to help me get a job in the customer service field. The experience was surprisingly fun and it yeilded more interviews. I started to think I might find a career I enjoyed in programming rather than using programming to help me find a job.
     </p>
     <p>
-      This text is so interesting. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo erat
-      eu viverra mollis. Quisque tellus ante, commodo nec tellus sit amet,
-      ornare blandit justo. Integer in cursus justo, vel aliquam ex. Etiam
-      ligula sapien, sagittis eu posuere et, tincidunt a arcu. Nullam egestas
-      lacus a molestie efficitur. Fusce neque mauris, commodo at lectus ut,
-      aliquet pretium neque. Cras laoreet sapien lorem, eu auctor lorem porta
-      quis. Proin placerat, nunc quis placerat mattis, ligula velit mollis nisi,
-      ac maximus velit urna vel nibh. Lorem ipsum dolor sit amet, consectetur
-      adipiscing elit.
+      In November of 2020 I started studying.
     </p>
   </PageTemplate>
 )
