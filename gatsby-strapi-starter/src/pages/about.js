03e5aea877733b8ff3bb4c7f84742cdb4681ac18
spaceC00kie <kirstie317@gmail.com>
2021-06-02 20:04:05 -0400

    about addition

--- gatsby-strapi-starter/src/pages/about.js
+++ gatsby-strapi-starter/src/pages/about.js
@@ -10,7 +10,7 @@ let About = () => (
     My journey began when I made a website to help me get a job in the customer service field. The experience was surprisingly fun and it yeilded more interviews. I started to think I might find a career I enjoyed in programming rather than using programming to help me find a job.
     </p>
     <p>
-      In November of 2020 I started studying.
+      In November of 2020 I started studying in earnest. It's been tough but rewarding and I want to turn this into a career.
     </p>
   </PageTemplate>
 )
