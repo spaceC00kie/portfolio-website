4d3b1f76f4c5d7b5906c4dbbb57b8545e5128dda
spaceC00kie <kirstie317@gmail.com>
2021-06-04 11:47:48 -0400

    about

--- gatsby-strapi-starter/src/pages/about.js
+++ gatsby-strapi-starter/src/pages/about.js
@@ -5,17 +5,15 @@ let About = () => (
   <PageTemplate title="About Me">
     <p>It's nice to meet you! My name is Kirstie and I'm a programmer.</p>
     <p>
-      I began my journey with making a simple website to help my job
-      search in the customer service field. The experience was surprisingly fun
-      and it yielded more interviews. I realized I could make programming my
-      career rather than using it to help me find another job.
+      I began my journey by making a personal website to support my job search in the customer service field. The experience of creating the site was surprisingly fun and it made me realize I could make programming itself my career.
     </p>
     <p>
-      In November of 2020 I started studying in earnest. It's been tough sometimes but rewarding.
+      In November of 2020 I started studying in earnest. It's been tough sometimes but rewarding. I started by learning Javascript, I completed a full stack project and created ... react to do list and keep plants alive
     </p>
     <p>Languages:</p>
     <p>Javascript</p>
     <p>Python</p>
+    <p>React</p>
   </PageTemplate>
 )
 
